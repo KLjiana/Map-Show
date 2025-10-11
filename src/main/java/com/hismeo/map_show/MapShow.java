3bc0c3c2f31014064e7318dc7dcd565af5ae0b11
KalebLangley <2436262516@qq.com>
2025-10-11 20:08:13 +0800

    init

---
+++ src/main/java/com/hismeo/map_show/MapShow.java
@@ -0,0 +1,15 @@
+package com.hismeo.map_show;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Mod(MapShow.MODID)
+public class MapShow {
+    public static final String MODID = "map_show";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+    public MapShow(IEventBus modEventBus, ModContainer modContainer){
+    }
+}
